66aacc85d3395c7638d0340bff8add45680e960a
maruta <maruta@giftee.co>
2018-05-09 14:31:08 +0900

    update goroutine

--- cmd/amex/main.go
+++ cmd/amex/main.go
@@ -6,10 +6,40 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/maruta-giftee/selenium-test/parser"
+	"github.com/maruta-giftee/selenium-test/selenium"
 	"github.com/maruta-giftee/selenium-test/selenium/chrome"
 )
+
+type scenario struct {
+	id  int
+	url string
+}
+
+func worker(scCh <-chan scenario, drvCh chan selenium.Driver) {
+	for sc := range scCh {
+		// New SeleniumDriver
+		driver := chrome.NewDriver()
+		if err := driver.Start(); err != nil {
+			log.Fatalf("Failed to start driver:%v", err)
+		}
+
+		// New Page from SeleniumDriver
+		page, err := driver.NewPage()
+		if err != nil {
+			log.Fatalf("Failed to open page:%v", err)
+			return
+		}
+		if err := page.Navigate(sc.url); err != nil {
+			log.Fatalf("Failed to navigate:%v", err)
+		}
+		page.Screenshot(fmt.Sprintf("./_tools/amex/output/%4d.png", sc.id))
+		log.Printf("OK: %d (%s)\n", sc.id, sc.url)
+		drvCh <- driver
+	}
+}
 
 func main() {
 	// Open file
@@ -23,20 +53,6 @@ func main() {
 	}
 	defer file.Close()
 
-	// New SeleniumDriver
-	driver := chrome.NewDriver()
-	if err := driver.Start(); err != nil {
-		log.Fatalf("Failed to start driver:%v", err)
-	}
-	defer driver.Stop()
-
-	// New Page from SeleniumDriver
-	page, err := driver.NewPage()
-	if err != nil {
-		log.Fatalf("Failed to open page:%v", err)
-		return
-	}
-
 	// Parse file by parser
 	inputs := parser.ParseList(file)
 	if err != nil {
@@ -44,12 +60,29 @@ func main() {
 		return
 	}
 
+	var wg sync.WaitGroup
+	scChan := make(chan scenario)
+	defer close(scChan)
+
+	drvChan := make(chan selenium.Driver)
+	defer close(drvChan)
+
+	go func() {
+		for {
+			driver := <-drvChan
+			driver.Stop()
+			wg.Done()
+		}
+	}()
+
+	for i := 1; i <= 15; i++ {
+		go worker(scChan, drvChan)
+	}
+
 	// run
 	for i, input := range inputs {
-		if err := page.Navigate(input); err != nil {
-			log.Fatalf("Failed to navigate:%v", err)
-		}
-		page.Screenshot(fmt.Sprintf("./_tools/amex/output/%4d.png", i))
-		log.Printf("OK: %d (%s)\n", i, input)
+		wg.Add(1)
+		scChan <- scenario{i, input}
 	}
+	wg.Wait()
 }
